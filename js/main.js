169d7be58d8b4aecb7d6ec82b98cedadca756f12
mvrush <mvrush@gmail.com>
2022-01-06 21:01:38 -0700

    Update Notes

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide"); 
 }
+
+// links for homepage weekly assignments
+// const links = [
+//     {
+//         label: "Week01 notes",
+//         url: "week1/index.html"
+//     }
+// ]
+// document.getElementById("assignmentLinks").textContent = links(label, url)
