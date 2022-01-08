0597f55a0970abf89f86a804658c1d78aa1f52e1
mvrush <mvrush@gmail.com>
2022-01-08 15:04:00 -0700

    Fix JavaScript Story Editor

--- js/story-editor.js
+++ js/story-editor.js
@@ -1,14 +1,14 @@
 function loadStory(){
-    var storyName = document.getElementById("name_input").value
-    var storyHTML = localStorage.getitem(storyName)
+    var storyName = document.getElementById("name_input").value // for an input or textarea, use the keyword 'value'
+    var storyHTML = localStorage.getItem(storyName)
     document.getElementById("story_editor").value = storyHTML
 }
 function saveStory() {
     var storyName = document.getElementById("name_input").value
     var storyHTML = document.getElementById("story_editor").value
-    localStorage.setitem(storyName, storyHTML)
+    localStorage.setItem(storyName, storyHTML) // local storage uses 'key-value pairs'. The keys are 'storyName' and 'storyHTML' and they have the values stored in them they got from the document
 }
 function displayStory() {
     var storyHTML = document.getElementById("story_editor").value
-    document.getElementById("story_display").innerHTML = storyHTML
+    document.getElementById("story_display").innerHTML = storyHTML // to show something to the user you use a section and the special word 'innerHTML'.
 }
