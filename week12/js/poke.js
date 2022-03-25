03fc4564171c620865d2278c5955fe8569b556ba
mvrush <mvrush@gmail.com>
2022-03-25 15:06:28 -0600

    successfully patch in pokemon api

--- week12/js/poke.js
+++ week12/js/poke.js
@@ -13,15 +13,18 @@ searchInput.addEventListener("input", (e) => {
     console.log("this is our 'value' const ->", value); // this checks to see if our event listener is listening to what's entered into our search input.
     console.log("this is our 'users' array created from our data.map() loop ->", users); // YOU CAN ONLY SEE THIS LOG AFTER TYPING INTO THE SEARCH BOX. It populates the 'users' array with the object returned from the .then(data) arrow function of our API call. It has the 'name', 'email' and 'element' returned for all the users.
     users.forEach(user => {
-        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value) // simply checks the user typed 'value' created from our event listener and if it matches the user name or email (converted to lower case using 'toLowerCase()'), it puts 'true' in the 'isVisible' const. So as soon as it detects a match, it changes to 'true'.
+        const isVisible = user.name.toLowerCase().includes(value) || user.url.toLowerCase().includes(value) // simply checks the user typed 'value' created from our event listener and if it matches the user name or email (converted to lower case using 'toLowerCase()'), it puts 'true' in the 'isVisible' const. So as soon as it detects a match, it changes to 'true'.
         user.element.classList.toggle("hide", !isVisible) // this line says; If 'isVisible' is false ('!isVisible'), toggle the "hide" class using the 'classList' method that adds a class to the list of classes on that 'element' which is our user card passed back up as the value of 'element' in our returned object from below.
     })
 })
 
-fetch("https://jsonplaceholder.typicode.com/users") // uses fetch() to call our API
-.then(res => res.json()) // recieves a response which it then uses an arrow function to convert to json using json()
-.then(data => {     // Then takes that json() which is filled with our 'data' from the API. We use arrow functions to manipulate the data.
-    users = data.map(user => { // this loops through the data pulling each 'user' using the 'map()' function and creates a card which it adds to our 'user' array defined above.
+// https://pokeapi.co/api/v2/pokemon?limit=100&offset=200
+// https://pokeapi.co/api/v2/pokemon
+fetch("https://pokeapi.co/api/v2/pokemon?limit=1126") // uses fetch() to call our API
+ .then(res => res.json()) // recieves a response which it then uses an arrow function to convert to json using json()
+ // .then(data => console.log("This is the API data we recieved ->", data))  // used to test whether we got our data back.
+    .then(data => {     // Then takes that json() which is filled with our 'data' from the API. We use arrow functions to manipulate the data.
+    users = data['results'].map(user => { // uses our data object and then pops down to the ['results'] array. Then loops through the data 'results' array (found in our returned object) pulling each 'pokemon' using the 'map()' function and creates a card which it adds to our 'user' array defined above.
     // This looks at the content from our HTML page template 'data-user-template' which is stored in our userCardTemplate variable
     const card = userCardTemplate.content.cloneNode(true).children[0]; // 'cloneNode(true)' clones all 'content' in the 'userCardTemplate' const. We then specify the first child node 'children[0]' and store it in the 'card' const
     // it returns our card template which you can see using console.log(card) to see the content stored in the 'card' const.
@@ -29,13 +32,13 @@ fetch("https://jsonplaceholder.typicode.com/users") // uses fetch() to call our 
     const header = card.querySelector("[data-header]"); // this is looking in our 'card' const. It is looking for the 'data-header' attribute listed in that <div> tag.   
     const body = card.querySelector("[data-body]"); // this is looking in our 'card' const. It is looking for the 'data-body' attribute listed in that <div> tag.
     header.textContent = user.name; // this looks at our 'user' object and gets the 'name'. It then stores it as 'textContent' and appends that to the 'header' const using the (.)
-    body.textContent = user.email;  // this looks at our 'user' object and gets the 'email'. It then stores it as 'textContent' and appends that to the 'header' const using the (.)
+    body.textContent = user.url;  // this looks at our 'user' object and gets the 'email'. It then stores it as 'textContent' and appends that to the 'header' const using the (.)
         // another way to say what's happening in the previous two lines is that it's looking at the 'header' or 'body' and appending the 'textContent' to that and the textContent = the 'name' or 'email' found in the 'user' object.
     userCardContainer.append(card); // This appends each card to our <div data-user-cards-container> held in our 'userCardContainer' const.
     // console.log("this is our 'data' ->", data); // logs our data that we received from the API which was turned into JSON data.
     // console.log("this is each 'user' from our 'data' created with the 'forEach()' loop ->", user);
     // console.log("this is what's in the 'header' const ->", header); // 
     // console.log("this is what's in the 'body' const ->", body);
-    return { name: user.name, email: user.email, element: card} // here we return an object. This object conatins our user name, user email, and the card element.
+    return { name: user.name, url: user.url, element: card} // here we return an object. This object conatins our user name, user email, and the card element.
     })
 })   
