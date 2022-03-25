781cae3e4d1ae6dde112d9a2276af5e834340b94
mvrush <mvrush@gmail.com>
2022-03-24 18:28:16 -0600

    Working on Block 2 Challenge access api

---
+++ week12/js/poke.js
@@ -0,0 +1,24 @@
+const userCardTemplate = document.querySelector("[data-user-template"); // looks at our html, queries the selector which is a <div> tag attribute, 'data-user-template' and puts that in our 'userCardTemplate' const.
+const userCardContainer = document.querySelector("[data-user-cards-container]"); // looks at our html, queries the selector which is a <div> tag attribute, 'data-user-cards-container' and creates the userCardContainer const.
+// console.log("this is our userCardTemplate const ->", userCardTemplate);
+console.log("this is our userCardContainer const ->", userCardContainer);
+
+fetch("https://jsonplaceholder.typicode.com/users") // uses fetch() to call our API
+.then(res => res.json()) // recieves a response which it then uses an arrow function to convert to json using json()
+.then(data => {     // Then takes that json() and stores it in a 'data' varible.
+    data.forEach(user => { // this loops through each 'user' using the 'forEach()' function and creates a card.
+    // This looks at the content from our HTML page template 'data-user-template' which is stored in our userCardTemplate variable
+    const card = userCardTemplate.content.cloneNode(true).children[0]; // 'cloneNode(true)' clones all 'content' in the 'userCardTemplate' const. We then specify the first child node 'children[0]' and store it in the 'card' const
+    // it returns our card template which you can see using console.log(card) to see the content stored in the 'card' const.
+    // console.log("this is our 'card' const", card);
+    const header = card.querySelector("[data-header]"); // this is looking in our 'card' const. It is looking for the 'data-header' attribute listed in that <div> tag.   
+    const body = card.querySelector("[data-body]"); // this is looking in our 'card' const. It is looking for the 'data-body' attribute listed in that <div> tag.
+    header.textContent = user.name; // this looks at our 'user' object and gets the 'name'. It then stores it as 'textContent' and appends that to the 'header' const using the (.)
+    body.textContent = user.email;  // this looks at our 'user' object and gets the 'email'. It then stores it as 'textContent' and appends that to the 'header' const using the (.)
+        // another way to say what's happening in the previous two lines is that it's looking at the 'header' or 'body' and appending the 'textContent' to that and the textContent = the 'name' or 'email' found in the 'user' object.
+    userCardContainer.append(card); // This appends each card to our <div data-user-cards-container> held in our 'userCardContainer' const.
+        // console.log("this is each 'user' from our 'data' created with the 'forEach()' loop", user);
+    // console.log("this is what's in the 'header' const ->", header); // 
+    // console.log("this is what's in the 'body' const ->", body);
+    })
+})   
